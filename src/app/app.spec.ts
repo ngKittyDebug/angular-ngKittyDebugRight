696ef3292317703e22941125e2df78d57156b600
dashque <zagorkiynahyi@gmail.com>
2026-05-08 20:55:06 +0300

    fix(tests): add coverage exclusion for html files

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,18 +1,20 @@
 import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { TuiRoot } from '@taiga-ui/core';
 import { TuiRootComponentMock } from '@shared/mocks/tui-root.component.mock/tui-root.component.mock';
+import { TuiRoot } from '@taiga-ui/core';
 
 describe('App', () => {
   let fixture: ComponentFixture<App>;
 
   beforeEach(async () => {
     TestBed.overrideComponent(App, {
+      add: {
+        imports: [TuiRootComponentMock],
+      },
       remove: {
         imports: [TuiRoot],
       },
-      add: { imports: [TuiRootComponentMock] },
     });
 
     await TestBed.configureTestingModule({
