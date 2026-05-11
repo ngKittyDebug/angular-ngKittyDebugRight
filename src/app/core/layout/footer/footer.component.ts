f8d46045dfd4687a0da8bcb6296f19f3f16902ce
Alena Alekseeva <a77993519@gmail.com>
2026-05-11 22:33:20 +0400

    feat: add basic code by footer

---
+++ src/app/core/layout/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngKitty-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+})
+export class FooterComponent {}
