ad73d7167fa6b15d742fc8fb3fd11a74483f3467
dashque <zagorkiynahyi@gmail.com>
2026-05-12 10:46:52 +0300

    feat:  start do layout component

---
+++ src/app/core/ui/components/layout/layout.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { TuiMainComponent } from '@taiga-ui/layout';
+
+@Component({
+  selector: 'ngKitty-layout',
+  imports: [RouterOutlet, TuiMainComponent],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {
+  // protected readonly navigationItemList = [
+  //   {
+  //     icon: '@tui.sparkles',
+  //     label: 'Таро',
+  //     url: '/t',
+  //   },
+  //   {
+  //     icon: '@tui.scroll',
+  //     label: 'Исповедь',
+  //     url: '/i',
+  //   },
+  //   {
+  //     icon: '@tui.flame',
+  //     label: 'Руны CI/CD',
+  //     url: '/r',
+  //   },
+  //   {
+  //     icon: '@tui.sun',
+  //     label: 'Гороскоп',
+  //     url: '/g',
+  //   },
+  //   {
+  //     icon: '@tui.book-open',
+  //     label: 'Хроники',
+  //     url: '/h',
+  //   },
+  // ] as const satisfies NavigationItem[];
+}
