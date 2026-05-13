758afbf0dc181039ae7ad02b035264839838886c
dashque <zagorkiynahyi@gmail.com>
2026-05-13 20:46:12 +0300

    feat: remove redundant import

--- src/app/core/ui/components/layout/layout.component.ts
+++ src/app/core/ui/components/layout/layout.component.ts
@@ -2,12 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TuiMainComponent } from '@taiga-ui/layout';
 import { FooterComponent } from '@core/ui/components/layout/footer/footer.component';
-import { SidebarComponent } from '@core/ui/components/layout/sidebar/sidebar.component';
 import { navigationItemListConfig } from '@core/ui/components/layout/sidebar/constants/navigation-item-list.config';
 
 @Component({
   selector: 'ngKitty-layout',
-  imports: [RouterOutlet, TuiMainComponent, FooterComponent, SidebarComponent],
+  imports: [RouterOutlet, TuiMainComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
