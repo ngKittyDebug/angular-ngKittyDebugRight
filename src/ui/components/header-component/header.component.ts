e6764cc3a68a26d5fc51a6d239eef51411e30be4
kozochkina82 <kozochkina82@yandex.ru>
2026-05-13 21:13:44 +0200

    feat: add card template to header

--- src/ui/components/header-component/header.component.ts
+++ src/ui/components/header-component/header.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { TuiButton, TuiIcon, TuiTitle } from '@taiga-ui/core';
 import { TuiHeader } from '@taiga-ui/layout';
-import { TuiAvatar } from '@taiga-ui/kit';
+import { TuiAutoColorPipe, TuiAvatar, TuiAvatarStack } from '@taiga-ui/kit';
+import { TuiCardMedium } from '@taiga-ui/layout';
 
 @Component({
   selector: 'header-component',
   standalone: true,
-  imports: [TuiTitle, TuiHeader, TuiButton, TuiIcon, TuiAvatar],
+  imports: [TuiTitle, TuiHeader, TuiButton, TuiIcon, TuiAutoColorPipe, TuiAvatar, TuiAvatarStack, TuiCardMedium],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,5 +17,5 @@ export class HeaderComponent {
   protected readonly title = 'Cult Of The Holy Deploy';
   protected readonly subtitle = '>> "Machine Spirit is unstable"<<';
 
-  protected readonly sizes = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'body-l', 'body-m', 'body-s'] as const;
+  protected readonly url = 'https://avatars.githubusercontent.com/u/70115541';
 }
