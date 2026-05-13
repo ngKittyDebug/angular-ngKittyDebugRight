b677f9f4ac4381ba255097895001da16bcc8238c
dashque <zagorkiynahyi@gmail.com>
2026-05-13 20:44:55 +0300

    feat: add navigation item list configuration for sidebar

--- src/app/core/ui/components/layout/layout.component.ts
+++ src/app/core/ui/components/layout/layout.component.ts
@@ -2,16 +2,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TuiMainComponent } from '@taiga-ui/layout';
 import { FooterComponent } from '@core/ui/components/layout/footer/footer.component';
-// import { SidebarComponent } from '@core/ui/components/layout/sidebar/sidebar.component';
-import { navigationItemList } from '@core/ui/components/layout/sidebar/fixtures/navigation-item-list.fixture';
+import { SidebarComponent } from '@core/ui/components/layout/sidebar/sidebar.component';
+import { navigationItemListConfig } from '@core/ui/components/layout/sidebar/constants/navigation-item-list.config';
 
 @Component({
   selector: 'ngKitty-layout',
-  imports: [RouterOutlet, TuiMainComponent, FooterComponent],
+  imports: [RouterOutlet, TuiMainComponent, FooterComponent, SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent {
-  protected readonly navigationItemList = navigationItemList;
+  protected readonly navigationItemList = navigationItemListConfig;
 }
--- src/app/core/ui/components/layout/sidebar/constants/navigation-item-list.config.ts
+++ src/app/core/ui/components/layout/sidebar/constants/navigation-item-list.config.ts
@@ -1,29 +1,29 @@
 import type { NavigationItem } from '@core/ui/components/layout/sidebar/models/navigation-item.model';
 
-export const navigationItemList = [
+export const navigationItemListConfig = [
   {
     icon: '@tui.sparkles',
     label: 'Таро',
-    url: '/t',
+    url: '/tarot',
   },
   {
     icon: '@tui.scroll',
     label: 'Исповедь',
-    url: '/i',
+    url: '/shrift',
   },
   {
     icon: '@tui.flame',
     label: 'Руны CI/CD',
-    url: '/r',
+    url: '/runes',
   },
   {
     icon: '@tui.sun',
     label: 'Гороскоп',
-    url: '/g',
+    url: '/horoscope',
   },
   {
     icon: '@tui.book-open',
     label: 'Хроники',
-    url: '/h',
+    url: '/chronicle',
   },
 ] as const satisfies NavigationItem[];
