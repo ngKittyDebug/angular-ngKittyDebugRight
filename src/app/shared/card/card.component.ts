366dbfe5e58c4f22611735ff24964e7c19d16442
Alena Alekseeva <a77993519@gmail.com>
2026-05-15 11:52:33 +0400

    feat: add card-elements

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,17 +1,20 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { TuiIcon, TuiTitle } from '@taiga-ui/core';
-import { TuiCardLarge, TuiSurface } from '@taiga-ui/layout';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { TuiButton, TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'ngKitty-card',
-  imports: [TuiCardLarge, TuiIcon, TuiSurface, TuiTitle],
+  imports: [TuiIcon, TuiButton],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
+  public readonly icon = input<string>();
   public readonly title = input<string>();
   public readonly subtitle = input<string>();
   public readonly decryption = input<string>();
-  public readonly icon = input<string>();
+  public readonly buttonLabel = input<string>();
+  public readonly badge = input<string>();
+
+  public readonly buttonClick = output<void>();
 }
