4803aaf8606d7eed1594dfc75edd6963733ad60e
Alena Alekseeva <a77993519@gmail.com>
2026-05-16 11:12:02 +0400

    feat: add 404-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import type { Routes } from '@angular/router';
 import { LayoutComponent } from '@core/ui/components/layout/layout.component';
+import { NotFoundComponent } from '@features/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
     children: [],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
