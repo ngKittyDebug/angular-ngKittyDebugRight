11693846f9a9fb17cf77b8153e83373e5be50f5b
kozochkina82 <kozochkina82@yandex.ru>
2026-05-19 22:47:31 +0200

    feat: add profile component

--- src/app/core/ui/components/layout/sidebar/sidebar.component.ts
+++ src/app/core/ui/components/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import type { NavigationItem } from '@core/ui/components/layout/models/navigation-item.model';
 import { NavigationItemComponent } from '@core/ui/components/layout/sidebar/navigation-item/navigation-item.component';
+import { RouterLink } from '@angular/router';
 import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'ngKitty-sidebar',
-  imports: [NavigationItemComponent, TranslocoModule],
+  imports: [NavigationItemComponent, RouterLink, TranslocoModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
