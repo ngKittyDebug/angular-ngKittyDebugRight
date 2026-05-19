61ba9082a6ce86fbd7666c20f609ec0ced52c470
Alena Alekseeva <a77993519@gmail.com>
2026-05-19 11:33:35 +0400

    feat: add transloco

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { TuiButton, TuiIcon } from '@taiga-ui/core';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'ngKitty-not-found',
-  imports: [TuiButton, TuiIcon],
+  imports: [TuiButton, TuiIcon, TranslocoDirective],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
