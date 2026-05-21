a5e5efe321efcfcf16230787fcfef11025643f89
kozochkina82 <kozochkina82@yandex.ru>
2026-05-21 19:16:05 +0200

    feat: add transloco directive (cherry-pick)

--- src/app/core/ui/components/layout/header/header.component.ts
+++ src/app/core/ui/components/layout/header/header.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TranslocoPipe } from '@jsverse/transloco';
+import { TranslocoModule } from '@jsverse/transloco';
 import { TuiButton, TuiIcon, TuiTitle } from '@taiga-ui/core';
 import { TuiHeader } from '@taiga-ui/layout';
 import { LanguageSwitcherComponent } from '@core/ui/components/language-switcher/language-switcher.component';
 
 @Component({
   selector: 'ngKitty-header',
-  imports: [TranslocoPipe, TuiTitle, TuiHeader, TuiButton, LanguageSwitcherComponent, TuiIcon],
+  imports: [TranslocoModule, TuiTitle, TuiHeader, TuiButton, LanguageSwitcherComponent, TuiIcon],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
