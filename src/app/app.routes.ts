c97afca2dd7741e391d304c9e88d3333b5dafc46
dashque <zagorkiynahyi@gmail.com>
2026-05-21 19:28:57 +0300

    feat(main): start do main page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import type { Routes } from '@angular/router';
 import { LayoutComponent } from '@core/ui/components/layout/layout.component';
 import { provideTranslocoScope } from '@jsverse/transloco';
+import { MainComponent } from '@features/main/main.component';
 
 export const routes: Routes = [
   {
@@ -8,8 +9,13 @@ export const routes: Routes = [
     component: LayoutComponent,
     children: [
       {
+        path: '',
+        component: MainComponent,
+        providers: [provideTranslocoScope('main')],
+      },
+      {
         path: '**',
-        loadComponent: () => import('./features/not-found/not-found.component').then((c) => c.NotFoundComponent),
+        loadComponent: () => import('@features/not-found/not-found.component').then((c) => c.NotFoundComponent),
         providers: [provideTranslocoScope('not-found')],
       },
     ],
