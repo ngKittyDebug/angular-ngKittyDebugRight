260c5a9f1ac46b51611ce842051e6ead7dc660d2
kozochkina82 <kozochkina82@yandex.ru>
2026-05-21 16:58:07 +0200

    feat: add profile component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import type { Routes } from '@angular/router';
 import { LayoutComponent } from '@core/ui/components/layout/layout.component';
+import { ProfileComponent } from '@core/ui/components/layout/profile/profile.component';
 import { provideTranslocoScope } from '@jsverse/transloco';
 
 export const routes: Routes = [
@@ -7,6 +8,10 @@ export const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: [
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      },
       {
         path: '**',
         loadComponent: () => import('./features/not-found/not-found.component').then((c) => c.NotFoundComponent),
