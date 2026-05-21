8af9aba9ba26779d790e67a908d83ecc27d7e8cb
dashque <zagorkiynahyi@gmail.com>
2026-05-21 19:51:02 +0300

    feat(header): add link to main in title

--- src/app/core/ui/components/layout/header/header.component.ts
+++ src/app/core/ui/components/layout/header/header.component.ts
@@ -3,10 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiButton, TuiIcon, TuiTitle } from '@taiga-ui/core';
 import { TuiHeader } from '@taiga-ui/layout';
 import { LanguageSwitcherComponent } from '@core/ui/components/language-switcher/language-switcher.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ngKitty-header',
-  imports: [TuiTitle, TuiHeader, TuiButton, LanguageSwitcherComponent, TuiIcon],
+  imports: [TuiTitle, TuiHeader, TuiButton, LanguageSwitcherComponent, TuiIcon, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
