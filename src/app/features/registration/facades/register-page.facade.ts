21c60627b8858260fd2158f9ec095380fc629140
dashque <zagorkiynahyi@gmail.com>
2026-05-23 16:53:34 +0300

    feat(registration): add registration page

--- src/app/features/registration/facades/register-page.facade.ts
+++ src/app/features/registration/facades/register-page.facade.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@angular/core';
+import { DestroyRef, inject, Injectable, signal } from '@angular/core';
+import { AuthService } from '@core/services/auth/auth.service';
+import { RegisterFormService } from '@features/registration/services/register-form.service';
+import { Router } from '@angular/router';
+import { TuiNotificationService } from '@taiga-ui/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TranslocoService } from '@jsverse/transloco';
 
 @Injectable()
-export class RegisterPageFacade {}
+export class RegisterPageFacade {
+  private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  private readonly notifications = inject(TuiNotificationService);
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly translocoService = inject(TranslocoService);
+  private readonly _isLoading = signal(false);
+  public readonly registerForm = inject(RegisterFormService).form;
+  public readonly isLoading = this._isLoading.asReadonly();
+
+  public async signup() {
+    if (this.registerForm().invalid() || this._isLoading()) {
+      return;
+    }
+
+    this._isLoading.set(true);
+
+    const { email, password, ...restRegisterData } = this.registerForm().value();
+
+    try {
+      await this.authService.signup(email, password, restRegisterData);
+
+      void this.showNotification(
+        this.translocoService.translate('notifications.login.success'),
+        this.translocoService.translate('notifications.login.success-title'),
+        'positive'
+      );
+      void this.router.navigate(['/']);
+    } catch (error) {
+      if (error instanceof Error) {
+        void this.showNotification(
+          error.message,
+          this.translocoService.translate('notifications.login.failure'),
+          'negative'
+        );
+      }
+    } finally {
+      this._isLoading.set(false);
+    }
+  }
+
+  public loginWithGithub() {
+    if (this._isLoading()) {
+      return;
+    }
+    this.authService.loginWithGithub();
+  }
+
+  public loginWithGoogle() {
+    if (this._isLoading()) {
+      return;
+    }
+    this.authService.loginWithGoogle();
+  }
+
+  private showNotification(message: string, label: string, appearance: 'positive' | 'negative') {
+    this.notifications
+      .open(message, {
+        label: label,
+        appearance: appearance,
+        autoClose: 5000,
+      })
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe();
+  }
+}
--- src/app/features/registration/ui/register-page/register-page.component.ts
+++ src/app/features/registration/ui/register-page/register-page.component.ts
@@ -1,10 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RegisterPageFacade } from '@features/registration/facades/register-page.facade';
+import { TuiInputPin, TuiPassword } from '@taiga-ui/kit';
+import { TranslocoPipe } from '@jsverse/transloco';
+import {
+  TuiButton,
+  TuiDateFormat,
+  TuiIcon,
+  TuiInputDirective,
+  TuiLoader,
+  tuiLoaderOptionsProvider,
+} from '@taiga-ui/core';
+import { RouterLink } from '@angular/router';
+import { FormField } from '@angular/forms/signals';
+import { TuiCardLarge, TuiForm } from '@taiga-ui/layout';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'ngKitty-register-page',
-  imports: [],
+  imports: [
+    TuiInputPin,
+    TranslocoPipe,
+    TuiInputDirective,
+    TuiPassword,
+    TuiButton,
+    TuiLoader,
+    RouterLink,
+    TuiIcon,
+    FormField,
+    TuiCardLarge,
+    TuiForm,
+    TuiDateFormat,
+  ],
+  providers: [tuiLoaderOptionsProvider({ size: 'm' })],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public readonly registerPageFacade = inject(RegisterPageFacade);
+  public readonly form = this.registerPageFacade.registerForm;
+  public readonly isLoading = this.registerPageFacade.isLoading;
+}
