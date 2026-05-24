1e9e40873635a2414fb45e55f30c59cfd2f51eb9
dashque <zagorkiynahyi@gmail.com>
2026-05-24 10:31:45 +0300

    feat(sidebar): add user email display and logout button

--- src/app/core/ui/components/layout/sidebar/sidebar.component.ts
+++ src/app/core/ui/components/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,13 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import type { NavigationItem } from '@core/ui/components/layout/models/navigation-item.model';
 import { NavigationItemComponent } from '@core/ui/components/layout/sidebar/navigation-item/navigation-item.component';
 import { TranslocoModule } from '@jsverse/transloco';
+import { AuthService } from '@core/services/auth/auth.service';
+import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'ngKitty-sidebar',
-  imports: [NavigationItemComponent, TranslocoModule],
+  imports: [NavigationItemComponent, TranslocoModule, TuiButton],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,4 +15,5 @@ import { TranslocoModule } from '@jsverse/transloco';
 export class SidebarComponent {
   public readonly navigationItemList = input.required<NavigationItem[]>();
   public readonly isMobileOpen = input(false);
+  protected readonly authService = inject(AuthService);
 }
