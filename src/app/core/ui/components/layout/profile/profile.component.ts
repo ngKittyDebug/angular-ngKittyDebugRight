84ef30c08fc1a0f86bc0b21b5800b2acc179310c
kozochkina82 <kozochkina82@yandex.ru>
2026-05-27 21:46:48 +0200

    feat: add transloco localisation

--- src/app/core/ui/components/layout/profile/profile.component.ts
+++ src/app/core/ui/components/layout/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiCardLarge } from '@taiga-ui/layout';
+import { TranslocoPipe } from '@jsverse/transloco';
 import { TuiAppearance, TuiIcon } from '@taiga-ui/core';
 import { TuiAvatar } from '@taiga-ui/kit';
 
 @Component({
   selector: 'ngKitty-profile',
-  imports: [TuiAppearance, TuiCardLarge, TuiAvatar, TuiIcon],
+  imports: [TuiAppearance, TuiCardLarge, TuiAvatar, TuiIcon, TranslocoPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
