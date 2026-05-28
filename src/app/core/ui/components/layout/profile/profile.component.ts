d0a92baf9ca37fc54c2a9425bc2433b19606b34a
kozochkina82 <kozochkina82@yandex.ru>
2026-05-28 20:18:53 +0200

    fix: add ng templates

--- src/app/core/ui/components/layout/profile/profile.component.ts
+++ src/app/core/ui/components/layout/profile/profile.component.ts
@@ -3,12 +3,69 @@ import { TuiCardLarge } from '@taiga-ui/layout';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { TuiAppearance, TuiIcon } from '@taiga-ui/core';
 import { TuiAvatar } from '@taiga-ui/kit';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'ngKitty-profile',
-  imports: [TuiAppearance, TuiCardLarge, TuiAvatar, TuiIcon, TranslocoPipe],
+  imports: [TuiAppearance, TuiCardLarge, TuiAvatar, TuiIcon, TranslocoPipe, NgTemplateOutlet],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  public stats = [
+    {
+      value: 0,
+      name: 'profile.confessions',
+    },
+    {
+      value: 0,
+      name: 'profile.writings',
+    },
+    {
+      value: 0.0,
+      name: 'profile.level',
+    },
+    {
+      value: 0,
+      name: 'profile.activity',
+    },
+  ];
+  public achieves = [
+    {
+      icon: './assets/pray.svg',
+      width: 30,
+      alt: 'pray',
+      title: 'profile.achieve.first_confession.title',
+      description: 'profile.achieve.first_confession.description',
+    },
+    {
+      icon: './assets/open-book-svgrepo-com.svg',
+      width: 35,
+      alt: 'book',
+      title: 'profile.achieve.chronicist.title',
+      description: 'profile.achieve.chronicist.description',
+    },
+    {
+      icon: './assets/happy.svg',
+      width: 30,
+      alt: 'smile',
+      title: 'profile.achieve.good_mood.title',
+      description: 'profile.achieve.good_mood.description',
+    },
+    {
+      icon: './assets/star.svg',
+      width: 35,
+      alt: 'star',
+      title: 'profile.achieve.regular_visitor.title',
+      description: 'profile.achieve.regular_visitor.description',
+    },
+    {
+      icon: './assets/crystal-ball.svg',
+      width: 35,
+      alt: 'magic ball',
+      title: 'profile.achieve.penitent.title',
+      description: 'profile.achieve.penitent.description',
+    },
+  ];
+}
