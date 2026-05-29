9a34cfc220b606f44d101d2264bcce3fbf3b6d7e
dashque <zagorkiynahyi@gmail.com>
2026-05-29 19:00:56 +0300

    refactor: use transloco service for translations

--- src/app/core/guards/dirty-form.guard.ts
+++ src/app/core/guards/dirty-form.guard.ts
@@ -3,13 +3,19 @@ import type { ComponentWithForm } from '@core/guards/models/component-with-form.
 import { inject } from '@angular/core';
 import { ModalService } from '@core/services/modal/modal.service';
 import { marker } from '@jsverse/transloco-keys-manager/marker';
+import { TranslocoService } from '@jsverse/transloco';
 
 export const dirtyFormGuard: CanDeactivateFn<ComponentWithForm> = (component) => {
   const modalService = inject(ModalService);
+  const translocoService = inject(TranslocoService);
 
   if (!component.form.dirty) {
     return true;
   }
 
-  return modalService.openConfirmModal(marker('modal.confirmation.content'), 's', marker('modal.confirmation.heading'));
+  return modalService.openConfirmModal(
+    translocoService.translate(marker('modal.confirmation.content')),
+    'm',
+    translocoService.translate(marker('modal.confirmation.heading'))
+  );
 };
