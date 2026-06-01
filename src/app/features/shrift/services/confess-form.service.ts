606558563baa9329370a93fb38f81c0b6f607ef4
Alena Alekseeva <a77993519@gmail.com>
2026-06-01 16:09:39 +0400

    feat: add confess form service

--- src/app/features/shrift/services/confess-form.service.ts
+++ src/app/features/shrift/services/confess-form.service.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import type { ConfessForm } from '../models/confess-form.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ConfessFormService {}
+export class ConfessFormService {
+  private readonly fb = inject(FormBuilder);
+  public confessForm = this.fb.group<ConfessForm>({
+    text: this.fb.nonNullable.control('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+    severity: this.fb.nonNullable.control('low'),
+  });
+}
