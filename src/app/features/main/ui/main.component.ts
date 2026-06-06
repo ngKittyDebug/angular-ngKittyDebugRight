1e1c8c2460156639a41e7498c8db874c44ee7527
dashque <zagorkiynahyi@gmail.com>
2026-06-06 21:53:51 +0300

    feat: add Oracle header

--- src/app/features/main/ui/main.component.ts
+++ src/app/features/main/ui/main.component.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject } from '@angular/core';
 import { CardComponent } from '@shared/ui/card/card.component';
 import { MainPageFacade } from '@features/main/facades/main-page.facade';
 import { TarotOracleComponent } from '@features/main/ui/tarot-oracle/tarot-oracle.component';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'ngKitty-main',
-  imports: [CardComponent, TarotOracleComponent],
+  imports: [CardComponent, TarotOracleComponent, TranslocoPipe],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
