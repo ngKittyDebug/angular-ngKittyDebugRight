d7bbd612ec536d57f104e5384878ef2f95ae7732
dashque <zagorkiynahyi@gmail.com>
2026-06-07 18:17:43 +0300

    refactor: update LayoutService to utilize UserProfileService for user management

--- src/app/core/services/layout/layout.service.mock.ts
+++ src/app/core/services/layout/layout.service.mock.ts
@@ -2,11 +2,11 @@ import type { MockedObject } from 'vitest';
 import { vi } from 'vitest';
 import type { LayoutService } from '@core/services/layout/layout.service';
 import type { Signal } from '@angular/core';
-import type { User } from 'firebase/auth';
+import type { UserProfile } from '@core/services/user-profile/models/user-profile.model';
 
 export const layoutServiceMock = {
   isMobileNavOpen: vi.fn().mockReturnValue(false) as unknown as Signal<boolean>,
-  user: vi.fn().mockReturnValue(null) as unknown as Signal<User | null>,
+  user: vi.fn().mockReturnValue(null) as unknown as Signal<UserProfile | null>,
   onToggleMobileNav: vi.fn(),
   onCloseMobileNav: vi.fn(),
   watchMobileNavMediaQuery: vi.fn(),
