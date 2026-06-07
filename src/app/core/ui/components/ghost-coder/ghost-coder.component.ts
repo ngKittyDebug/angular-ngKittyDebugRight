b27357151748cacd6aaccf529c29a4e9b8792a50
Alena Alekseeva <a77993519@gmail.com>
2026-06-07 20:54:01 +0400

    feat: create ghost-coder component

---
+++ src/app/core/ui/components/ghost-coder/ghost-coder.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngKitty-ghost-coder',
+  imports: [],
+  templateUrl: './ghost-coder.component.html',
+  styleUrl: './ghost-coder.component.scss',
+})
+export class GhostCoderComponent {}
