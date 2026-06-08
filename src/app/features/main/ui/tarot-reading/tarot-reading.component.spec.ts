4dc55f25fe5c278f888a43d4c8033bbe10cc06ed
dashque <zagorkiynahyi@gmail.com>
2026-06-08 18:46:02 +0300

    feat(main): fix tests

--- src/app/features/main/ui/tarot-reading/tarot-reading.component.spec.ts
+++ src/app/features/main/ui/tarot-reading/tarot-reading.component.spec.ts
@@ -2,6 +2,7 @@ import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { TarotReadingComponent } from './tarot-reading.component';
+import { tarotResponseApiFixture } from '@features/main/data/api/fixtures/tarot-response-api.fixture';
 
 describe('CardViewComponent', () => {
   let component: TarotReadingComponent;
@@ -14,6 +15,7 @@ describe('CardViewComponent', () => {
 
     fixture = TestBed.createComponent(TarotReadingComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('tarotReading', tarotResponseApiFixture);
     fixture.detectChanges();
   });
 
