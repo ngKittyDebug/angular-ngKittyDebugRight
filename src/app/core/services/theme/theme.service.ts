5ed0810a45fcc6954f0a0669730fe660516ff636
dashque <zagorkiynahyi@gmail.com>
2026-06-19 19:41:51 +0300

    refactor(theme): update ThemeService to use inject and computed for theme management

--- src/app/core/services/theme/theme.service.ts
+++ src/app/core/services/theme/theme.service.ts
@@ -1,14 +1,13 @@
-import { Service, signal } from '@angular/core';
+import { computed, inject, Service } from '@angular/core';
+import { TUI_DARK_MODE } from '@taiga-ui/core';
 import { Theme } from '@core/models/theme.model';
 
 @Service()
 export class ThemeService {
-  private readonly _theme = signal<Theme>(Theme.DARK);
-  public readonly theme = this._theme.asReadonly();
+  private readonly _theme = inject(TUI_DARK_MODE);
+  public readonly theme = computed(() => (this._theme() ? Theme.DARK : Theme.LIGHT));
 
   public toggleTheme() {
-    this._theme.update((currentTheme) => {
-      return currentTheme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-    });
+    this._theme.set(!this._theme());
   }
 }
