6cffe4fbcb7cd68fe8688e8a7c2b57a183a40ca6
dashque <zagorkiynahyi@gmail.com>
2026-06-19 19:45:56 +0300

    refactor(theme): update tests

--- src/app/core/services/theme/theme.service.spec.ts
+++ src/app/core/services/theme/theme.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { TUI_DARK_MODE } from '@taiga-ui/core';
+import { createTuiDarkModeMock } from '@shared/mocks/tui-dark-mode/tui-dark-mode.mock';
 
 import { ThemeService } from './theme.service';
 
@@ -6,7 +8,9 @@ describe('ThemeService', () => {
   let service: ThemeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ThemeService, { provide: TUI_DARK_MODE, useValue: createTuiDarkModeMock() }],
+    });
     service = TestBed.inject(ThemeService);
   });
 
