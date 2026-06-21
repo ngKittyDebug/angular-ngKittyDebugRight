8c603d036cadfcba8feef08168030d301624c142
Alena Alekseeva <a77993519@gmail.com>
2026-06-21 18:32:06 +0400

    feat: add ball component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -65,6 +65,12 @@ export const routes: Routes = [
         data: { preloadFor: PreloadFor.AUTH },
       },
       {
+        path: 'crystal-ball',
+        canMatch: [authGuard],
+        loadComponent: () => import('./features/ball/ui/ball.component').then((c) => c.BallComponent),
+        data: { preloadFor: PreloadFor.AUTH },
+      },
+      {
         path: '**',
         loadComponent: () => import('./features/not-found/not-found.component').then((c) => c.NotFoundComponent),
         providers: [provideTranslocoScope('not-found')],
---
+++ src/app/features/ball/ui/ball.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngKitty-ball',
+  imports: [],
+  templateUrl: './ball.component.html',
+  styleUrl: './ball.component.scss',
+})
+export class BallComponent {}
